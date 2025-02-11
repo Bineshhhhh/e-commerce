838c7fa61600e8c41a0924ba673de3ebf3514d2d
Bineshhhhh <104159095+Bineshhhhh@users.noreply.github.com>
2025-02-11 15:35:25 +0930

    category fixed

--- src/pages/ShopCategory.jsx
+++ src/pages/ShopCategory.jsx
@@ -7,11 +7,9 @@ import Item from '../components/Items/Item'
 const ShopCategory = (props) => {
     const {all_product} = useContext(ShopContext);
 
-    console.log("Banner URL:", props.banner); //Debuggung banner prop
-
   return (
     <div className='shop-category'>
-      <img className='shop-category-banner' src={props.banner} alt="" /> 
+      <img className='shopcategory-banner' src={props.banner} alt="" /> 
       <div className="shopcategory-indexSort">
         <p>
             <span>Showing 12</span> of 36 products
@@ -28,7 +26,9 @@ const ShopCategory = (props) => {
                     return null;
                 }
             })}
-
+        </div>
+        <div className="shopcategory-loadmore">
+          Explore More
         </div>
       </div>
     </div>
